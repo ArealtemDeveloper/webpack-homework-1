4021eedb454b01bce6a39653aa50187ed20c2b92
Alexandr Belov <a.belov@idaproject.com>
2024-12-28 13:00:43 +0500

    feat: добавил поддержку typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,16 @@ module.exports = {
     },
     extends: [
         'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
+    parser: '@typescript-eslint/parser', 
     parserOptions: {
         ecmaVersion: 12,
         sourceType: 'module',
     },
+    plugins: [
+        '@typescript-eslint',
+    ],
     rules: {
         'indent': ['error', 4],
         'linebreak-style': ['error', 'unix'],
